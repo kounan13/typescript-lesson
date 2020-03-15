c5f4696dc15b9f2f1338b844cd005734c1d1e5f9
kounan13 <yujishono@hotmail.com>
2020-03-15 23:07:52 +0900

    Initialize repository

---
+++ hello.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+//axios.get('https://google.com', {
+//    headers: 
+//});
+var hello = 'hello2';
+console.log(hello + 'a' + add(1, 2));
+function add(a, b) {
+    return a + b;
+}
---
+++ hello.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+//axios.get('https://google.com', {
+//    headers: 
+//});
+
+let hello: string = 'hello2';
+console.log(hello + 'a' + add(1, 2));
+
+function add(a: number, b: number): number {
+    return a + b;
+}
